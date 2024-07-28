4a903d178eabf27864315bcc0bcb8f4107e3556b
everton-guilherme-morais <guilherme20.silva99@gmail.com>
2024-07-28 15:16:41 -0300

    adjust for deploy

--- app/searchProducts/page.tsx
+++ app/searchProducts/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import StarRating from "@/components/StarsRating";
 import Image from "next/image";
 import { toast, useToast } from "@/components/ui/use-toast";
@@ -49,46 +49,48 @@ export default function SearchProducts() {
   
 
   return (
-    <section className="container mx-auto mb-20">
-      <div className="flex flex-1 flex-col gap-10">
-        <div className="flex flex-row items-center gap-3">
-          <h2 className="font-semibold text-xl">{message}</h2>
-          <span className="text-opacity-50 text-gray-600">{`${products.length} produtos encontrados`}</span>
-        </div>
-        {loading ? (
-          <div className="flex items-center justify-center w-full h-[150px] font-medium">Carregando...</div>
-        ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-            {products.map((product) => (
-              <div key={product.id} className="flex flex-col h-full w-full items-baseline justify-center gap-5">
-                <div className='relative'>
-                  <Image
-                    src={product.imageCover}
-                    alt={product.name}
-                    className="block h-full w-full object-cover rounded-xl"
-                    width={250}
-                    height={300}
-                  />
-                  <Link href={`/detailsProduct/${product.id}`} className='absolute top-[90%] left-[80%] bg-[#f48646] rounded-full p-3 cursor-pointer'>
-                    <ShoppingBag className='text-white' />
-                  </Link>
-                </div>
-                <div className="flex flex-col items-start w-auto">
-                  <h1 className="flex font-bold">{product.name}</h1>
-                  <div className="flex flex-row gap-2">
-                    <StarRating stars={product.stars} />
-                    {product.stars} de 5
-                  </div>
-                  <div className="flex flex-row justify-between gap-3">
-                    <span className="font-extrabold text-xl"> R$ <b>{product.priceWithDiscount}</b></span>
-                    <s>R$ {product.priceInitial}</s>
-                  </div>
-                </div>
-              </div>
-            ))}
+    <Suspense fallback={<div>Loading...</div>}>
+      <section className="container mx-auto mb-20">
+        <div className="flex flex-1 flex-col gap-10">
+          <div className="flex flex-row items-center gap-3">
+            <h2 className="font-semibold text-xl">{message}</h2>
+            <span className="text-opacity-50 text-gray-600">{`${products.length} produtos encontrados`}</span>
           </div>
-        )}
-      </div>
-    </section>
+          {loading ? (
+            <div className="flex items-center justify-center w-full h-[150px] font-medium">Carregando...</div>
+          ) : (
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+              {products.map((product) => (
+                <div key={product.id} className="flex flex-col h-full w-full items-baseline justify-center gap-5">
+                  <div className='relative'>
+                    <Image
+                      src={product.imageCover}
+                      alt={product.name}
+                      className="block h-full w-full object-cover rounded-xl"
+                      width={250}
+                      height={300}
+                    />
+                    <Link href={`/detailsProduct/${product.id}`} className='absolute top-[90%] left-[80%] bg-[#f48646] rounded-full p-3 cursor-pointer'>
+                      <ShoppingBag className='text-white' />
+                    </Link>
+                  </div>
+                  <div className="flex flex-col items-start w-auto">
+                    <h1 className="flex font-bold">{product.name}</h1>
+                    <div className="flex flex-row gap-2">
+                      <StarRating stars={product.stars} />
+                      {product.stars} de 5
+                    </div>
+                    <div className="flex flex-row justify-between gap-3">
+                      <span className="font-extrabold text-xl"> R$ <b>{product.priceWithDiscount}</b></span>
+                      <s>R$ {product.priceInitial}</s>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Suspense>
   )
 }
