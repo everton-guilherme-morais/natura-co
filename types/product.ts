0df61490a463d3d1a0290a913b2007b7c584311d
everton-guilherme-morais <guilherme20.silva99@gmail.com>
2024-07-29 00:25:48 -0300

    remove comment

--- types/product.ts
+++ types/product.ts
@@ -1,20 +1,20 @@
 export interface Product {
   id: number;
-  name: string; /*Nome*/
-  brand: string; /*Marca*/
-  imageCover: string; /*Imagem principal*/
-  stars: number; /*Estrelas*/
-  discount?: number; /*Desconto*/
-  category: string; /*Categoria*/
-  description: string; /*Descrição*/
-  size?: string | null; /*Tamanho*/
-  instalments: number; /*Parcelas*/
+  name: string;
+  brand: string;
+  imageCover: string;
+  stars: number;
+  discount?: number;
+  category: string;
+  description: string;
+  size?: string | null;
+  instalments: number;
   assessments?: {
     id: number, name: string, comment: string, stars: number, dateCommment: string,
-  }[]; /*Avaliações*/
-  goodToKnow?: { id: number; description: string }[]; /*É bom você saber*/
-  priceInitial: string, /*Preço inicial (sem desconto)*/
-  priceWithDiscount: string, /*Preço com desconto*/
-  quantity: number; /*Quantidade*/
-  stateProduct?: string, /*Estado do produto*/
+  }[];
+  goodToKnow?: { id: number; description: string }[];
+  priceInitial: string,
+  priceWithDiscount: string,
+  quantity: number;
+  stateProduct?: string,
 }
